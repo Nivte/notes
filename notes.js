325edfc13eabb24ee892bfa9d1e1b100e0699b0d
Niv Tene <niv.tene@gmail.com>
2023-07-24 03:17:36 +0300

    almostDone

--- notes.js
+++ notes.js
@@ -1,4 +1,33 @@
-let noteArr = [];
+// new variable that contains the stored data
+let savedNotes = localStorage.getItem("notes");
+// the array's data is the stored data + restoring new Date function that was 'broken', with its original data
+let noteArr = savedNotes
+  ? JSON.parse(savedNotes).map((item) => ({
+      ...item,
+      dateCreated: new Date(item.dateCreated),
+    }))
+  : [];
+console.log(noteArr);
+
+// function that saves the array's data for next time
+function save() {
+  localStorage.setItem("notes", JSON.stringify(noteArr));
+}
+
+// id save
+let n = localStorage.getItem("currentId")
+  ? Number(localStorage.getItem("currentId"))
+  : 0;
+
+function saveId() {
+  localStorage.setItem("currentId", n);
+}
+
+function getNotes() {
+  console.log(noteArr);
+  save();
+  saveId();
+}
 
 let $zone = document.getElementById("zone");
 
@@ -6,23 +35,60 @@ let $addNoteBtn = document.getElementById("addNoteBtn");
 
 $addNoteBtn.addEventListener("click", addNote);
 
-function getNotes() {
-  console.log(noteArr);
-}
-getNotes();
-
-let n = 0;
-
 function addNote() {
-  let singleNote = { id: n };
+  let singleNote = { Id: n, dateCreated: new Date(), userWrote: "" };
   noteArr.push(singleNote);
-  console.log(noteArr);
-
   renderAddNote();
   n++;
+  getNotes();
+
+  // renderNoteZone();
 }
 
 function deleteNote() {
-  noteArr.splice(event.target.id[6], 1);
+  let deleted = event.target.id[7]
+    ? String(event.target.id[6]) + String(event.target.id[7])
+    : event.target.id[6];
+
+  if (noteArr.find((x) => x.Id == deleted)) {
+    noteArr.splice(
+      noteArr.findIndex((x) => x.Id == deleted),
+      1
+    );
+  }
+  // console.log(noteArr.findIndex(x=>x.Id==event.target.id[6]))
+  save();
+  saveId();
+  getNotes();
+  renderDelNote();
+}
+
+function delAllNotes() {
+  localStorage.removeItem("currentId");
+  n = 0;
+  localStorage.removeItem("notes");
+  noteArr = [];
+  save();
+  saveId();
+  renderDelAll();
+}
+
+// localStorage.clear();
+
+function saveValue() {
+  let written = document.getElementById(
+    `textBox${
+      event.target.id[13]
+        ? event.target.id[12] + event.target.id[13]
+        : event.target.id[12]
+    }`
+  );
+  noteArr[
+    event.target.id[13]
+      ? event.target.id[12] + event.target.id[13]
+      : event.target.id[12]
+  ].userWrote = written.value;
   console.log(noteArr);
+  save();
+  saveId();
 }
--- render.js
+++ render.js
@@ -1,15 +1,159 @@
-function renderAddNote() {
-  $zone.innerHTML += `       <div id="note${n}" class="note">
-  <textarea name="" id="textBox" cols="30" rows="10">
+function convert() {
+  noteArr.length == 0 ? "" : (n = 0);
 
+  for (i = 0; i < noteArr.length; i++) {
+    $zone.innerHTML += `  
+
+  
+ 
+       <div id="note${n}" class="note">
+ 
+       <p class="date">${noteArr[noteArr.length - 1].dateCreated.getDate()}/ ${
+      noteArr[noteArr.length - 1].dateCreated.getMonth() + 1
+    }
+       ${noteArr[noteArr.length - 1].dateCreated.getFullYear()}</p>
+     
+   <textarea name="" id="textBox${n}" class="textBox" cols="30" rows="10">
+${noteArr[n].userWrote}
+   </textarea>
+  
+   <button onclick="deleteNote()" id="delBtn${n}" class="delBtn" >Delete</button>
+   <button onclick="saveValue()" id="saveValueBtn${n}" class="saveValueBtn">Save Value</button>
+   <div class="serial"> ${n} </div>
+ </div>`;
+    n++;
+  }
+
+  console.log($zone);
+}
+
+convert();
+// localStorage.clear()
+
+function renderAddNote() {
+  $zone.innerHTML += `  
+
+ 
+
+      <div id="note${n}" class="note">
+    
+      <p class="date">${noteArr[noteArr.length - 1].dateCreated.getDate()}/ ${
+    noteArr[noteArr.length - 1].dateCreated.getMonth() + 1
+  }
+          ${noteArr[noteArr.length - 1].dateCreated.getFullYear()}</p>
+    
+  <textarea name="" id="textBox${n}" class="textBox" cols="30" rows="10">
+  ${noteArr[n].userWrote}
   </textarea>
  
-  <button onclick="deleteNote()" id="delBtn${n}" class="delBtn" >delete</button>
+  <button onclick="deleteNote()" id="delBtn${n}" class="delBtn" >Delete</button>
+  <button onclick="saveValue()" id="saveValueBtn${n}" class="saveValueBtn">Save Value</button>
+  <div class="serial"> ${n} </div>
 </div>`;
 }
 
 function renderDelNote() {
-  
-   
-  
+  let deletedR = event.target.closest("div");
+  $zone.removeChild(deletedR);
+  console.log(n);
+  return deletedR;
 }
+
+// localStorage.clear()
+
+// let html = localStorage.getItem("notesRender")
+//   ? localStorage.getItem("notesRender")
+//   : `<div id="zone" class="zone">`;
+
+// function renderNoteZone() {
+//   html += renderAddNote();
+//   html + `</div>`;
+//   // console.dir(html);
+//   saveRender();
+//   renderPage();
+// }
+
+// let deletedR = "";
+// function renderDelNote() {
+// deletedR = event.target.closest("div");
+// deletedR = event.target.closest("div");
+// html.remove(deletedR)
+// // html.removeChild(deletedR);
+// deletedR = "";
+// let deleted = event.target.closest("div");
+// console.log((deleted));
+// html.replace(deleted, "");
+// html.slice(deleted);
+// console.log(html);
+// }
+// console.log(html);
+function renderDelAll() {
+  $zone.innerHTML = "";
+}
+
+// function saveRender() {
+//   localStorage.setItem("notesRender", html);
+// }
+
+// console.log(localStorage.getItem("notesRender"));
+
+// function saveRender() {
+//   localStorage.setItem(
+//     "notesRender",
+//     `
+
+//   <p class="date">${noteArr[noteArr.length - 1].dateCreated.getDate()}/ ${
+//       noteArr[noteArr.length - 1].dateCreated.getMonth() + 1
+//     }
+//   ${noteArr[noteArr.length - 1].dateCreated.getFullYear()}</p>
+
+//       <div id="note${n}" class="note">
+
+//   <textarea name="" id="textBox" cols="30" rows="10">
+
+//   </textarea>
+
+//   <button onclick="deleteNote()" id="delBtn${n}" class="delBtn" >delete</button>
+//   <div class="serial"> ${n} </div>
+// </div>`
+
+//   );
+
+//   //   let html = "";
+//   // html=JSON.stringify($zone)
+//   //   console.log(html);
+// }
+
+// saveRender();
+
+// deletedR = document.getElementById(`note${event.target.id[6]}`);
+
+// $zone.removeChild(deletedR);
+
+// deletedR = "";
+//  deletedR = console.log(event.target.closest("div"));
+//   $zone.removeChild(deletedR)
+
+// let $body = document.getElementById("body");
+// console.log($body);
+
+//test
+
+// let a = document.createElement("button");
+
+// console.log(a);
+
+// $body.appendChild(a);
+
+// function test() {
+// $body.removeChild("<button></button>")
+
+// }
+
+// localStorage.clear()
+
+let field = document.getElementById("textBox1");
+
+// function findValue() {
+//   console.dir((field.value = "mmmm"));
+// }
